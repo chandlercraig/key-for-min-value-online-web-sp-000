4e7d056db56420e3ae4bec912564b6f99914daff
Chandler Craig <hellochandlercraig@gmail.com>
2020-11-25 16:21:58 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,12 +2,12 @@ def key_for_min_value(name_hash)
   if name_hash == {}
     return nil
   else
-   min_value = Float::INFINITY
-   key_for_min_value = "hi"
+   min_val = Float::INFINITY
+   key_for_min_val = "hi"
    name_hash.each do |name, age|
      if age < min_value
        min_val = age
-       key_for_min_value = name
+       key_for_min_val = name
    end
   end
  end
