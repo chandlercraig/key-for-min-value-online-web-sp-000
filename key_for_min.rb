6a8e7956b19a3eab5ff392ee5d6a97d1dee09ae1
Chandler Craig <hellochandlercraig@gmail.com>
2020-11-25 15:39:16 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,8 +3,9 @@
 
 def key_for_min_value(name_hash)
   if name_hash == (!{})
-    name_hash.each do |key, value|
-      
+    age_arr = []
+    name_hash.collect {|key, value|
+
     end
   else
     nil
