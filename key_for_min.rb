f5c927ff35c2e4b497d107d297e45165b27017db
Chandler Craig <hellochandlercraig@gmail.com>
2020-11-25 16:17:47 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,10 +1,10 @@
-# prereqs: iterators, hashes, conditional logic
-# Given a hash with numeric values, return the key for the smallest value
+require 'pry'
 
 def key_for_min_value(name_hash)
   if name_hash == {}
     return nil
   else
+    binding.pry
    min_value = Float::INFINITY
    key_for_min_value = "hi"
    name_hash.each do |name, age|
