ecb01c4b9ee35ea1f3cd6195844c8281d2e97f43
Chandler Craig <hellochandlercraig@gmail.com>
2020-11-25 15:57:54 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -6,7 +6,6 @@
 #end of the loop, return key_for_min_value.
 
 def key_for_min_value(name_hash)
-  if name_hash == !{}
    min_value = Float:INFINITY
    key_for_min_value = "hi"
    name_hash.each do |name, age|
@@ -16,7 +15,4 @@ def key_for_min_value(name_hash)
      end
    end
    return key_for_min_value
-  else
-    nil
-  end
 end
