78269278bbed4b5e79b4b3dfda3c21fe3a47bfc5
Chandler Craig <hellochandlercraig@gmail.com>
2020-11-25 16:20:50 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,10 +1,7 @@
-require 'pry'
-
 def key_for_min_value(name_hash)
   if name_hash == {}
     return nil
   else
-    binding.pry
    min_value = Float::INFINITY
    key_for_min_value = "hi"
    name_hash.each do |name, age|
