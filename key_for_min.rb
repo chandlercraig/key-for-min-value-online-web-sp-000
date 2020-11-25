cfe5d0b297bb11409ca1f2312d3c46ad9afd8cfb
Chandler Craig <hellochandlercraig@gmail.com>
2020-11-25 16:02:26 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,10 +1,6 @@
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
 
-#Track min_value and key_for_min_value. Iterate through the hash, and any time
-#the current value is lower than min_value, update both of these vars. At the
-#end of the loop, return key_for_min_value.
-
 def key_for_min_value(name_hash)
   if name_hash == {}
     return nil
